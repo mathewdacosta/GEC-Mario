791f6477ba2d06ef2c13f6db74165ad1bfc6766f
Mathew Dacosta <72082814+mathewdacosta@users.noreply.github.com>
2021-02-12 11:21:16 +0000

    Tweak gravity parameters

--- GEC-Mario/constants.h
+++ GEC-Mario/constants.h
@@ -5,6 +5,6 @@
 #define SCREEN_HEIGHT 416
 
 // Character movement characteristics
-const float GRAVITY = 260.0f;
+const float GRAVITY = 220.0f;
 const float JUMP_FORCE_DECREMENT = 600.0f;
-const float INITIAL_JUMP_FORCE = 600.0f;
+const float INITIAL_JUMP_FORCE = 300.0f;
