847911138ef60ed14da786c9e865385ac8e977d1
Mathew Dacosta <72082814+mathewdacosta@users.noreply.github.com>
2021-04-11 17:52:11 +0100

    Fix Mario and Luigi sharing movement speed

--- GEC-Mario/CharacterMario.cpp
+++ GEC-Mario/CharacterMario.cpp
@@ -1,7 +1,7 @@
 #include "CharacterMario.h"
 
 CharacterMario::CharacterMario(SDL_Renderer* renderer, Vector2D start_position, LevelMap* map, SoundEffect* jump_sound) :
-	Player(renderer, "Images/Mario.png", start_position, LUIGI_MOVE_SPEED, INITIAL_JUMP_FORCE, 2, 16.0f, map, jump_sound)
+	Player(renderer, "Images/Mario.png", start_position, MARIO_MOVE_SPEED, INITIAL_JUMP_FORCE, 2, 16.0f, map, jump_sound)
 {
 }
 
